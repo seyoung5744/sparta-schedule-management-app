30123c47ca2a244d2ea2f1b4e5afbeba01265a89
seyoung5744 <seyoung574458@gmail.com>
2025-08-01 13:27:21 +0900

    create project

---
+++ src/main/java/com/example/spartaschedulemanagement/SpartaScheduleManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.spartaschedulemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaScheduleManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpartaScheduleManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spartaschedulemanagement/SpartaScheduleManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spartaschedulemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaScheduleManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
